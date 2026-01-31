dd820314e38866d4bfe875b0629a10df2a287e94
Akash4701 <saha.akash4701@gmail.com>
2026-01-31 11:06:40 +0530

    rectified components

--- app/pages/Home.tsx
+++ app/pages/Home.tsx
@@ -4,7 +4,7 @@ import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import { Cinzel_Decorative, Eagle_Lake } from "next/font/google";
 import { motion, AnimatePresence } from "framer-motion";
-import MahabharataNavbar from "../components/Navbar";
+import MahabharataNavbar from "../../components/Navbar";
 
 interface TimeLeft {
   days: number; hours: number; minutes: number; seconds: number;
