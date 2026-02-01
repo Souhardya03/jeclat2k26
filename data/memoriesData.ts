6b53f1c6a786e2ce38ceee5070d93bff84347329
Souhardya20 <souhardyadeb2016@gmail.com>
2026-02-02 01:42:04 +0530

    update

---
+++ data/memoriesData.ts
@@ -0,0 +1,26 @@
+export const memoriesData = [
+  {
+    title: 'A Glimpse Of Jeclat 2k23',
+    year: '2023',
+    youtubeLink:
+      'https://www.youtube-nocookie.com/embed/LIVV85YfwTQ?si=AewcEauEJ4FtwQ9Q',
+  },
+  {
+    title: 'A Glimpse Of Jeclat 2k22',
+    year: '2022',
+    youtubeLink:
+      'https://www.youtube.com/embed/cQc-4M6KWHc?si=FF69DuO1caKWnogm',
+  },
+  {
+    title: 'A Glimpse Of Jeclat 2k20',
+    year: '2020',
+    youtubeLink:
+      'https://www.youtube.com/embed/4h9POxBsqS4?si=gLRJDJU8aZtzUH5J',
+  },
+  {
+    title: 'A Glimpse Of Jeclat 2k18',
+    year: '2018',
+    youtubeLink:
+      'https://www.youtube.com/embed/5HtPuIoEsFU?si=jlU_szYjn-WQNF6p',
+  },
+];
