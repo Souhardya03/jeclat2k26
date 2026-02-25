0d1f4ec0174c80e41c0c639d7105f42086d4dafc
Souhardya Deb <104934508+Souhardya03@users.noreply.github.com>
2026-02-25 20:16:27 +0530

    Update eventData.ts

--- data/eventData.ts
+++ data/eventData.ts
@@ -188,7 +188,7 @@ export const eventData = [
     type: "competitive-event",
     image: tshirt_painting,
     maxMembers: 2,
-    registrationOpen: false,
+    registrationOpen: true,
     eventInfo: {
       title: "T-Shirt Painting",
       description: "Creative t-shirt painting competition",
